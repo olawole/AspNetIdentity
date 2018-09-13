8db36e6f00ba1215656c12b54115d6c3a705f79d
perfectedchild <olawolexyoni@gmail.com>
2018-09-14 00:08:01 +0100

    ApplicationManger added

---
+++ ASPNetIdentity/Controllers/AccountsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ASPNetIdentity.Controllers
+{
+    [RoutePrefix("api/accounts")]
+    public class AccountsController : ApiController
+    {
+        [Route("users")]
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(this.AppUserManager.Users.ToList().Select(u => this.TheModelFactory.Create(u)));
+        }
+    }
+}
---
+++ ASPNetIdentity/Startup.cs
@@ -0,0 +1,42 @@
+﻿using ASPNetIdentity.Infrastructure;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+
+namespace ASPNetIdentity
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration httpConfig = new HttpConfiguration();
+
+            ConfigureOAuthTokenGeneration(app);
+
+            ConfigureWebApi(httpConfig);
+
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            app.UseWebApi(httpConfig);
+        }
+
+        private void ConfigureOAuthTokenGeneration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext(ApplicationDbContext.Create);
+            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+        }
+
+        private void ConfigureWebApi(HttpConfiguration config)
+        {
+            config.MapHttpAttributeRoutes();
+
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
